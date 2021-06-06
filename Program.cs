4749bc5da98e91fa245de01f29f9be7263f7e91d
Northman94 <Zhandarm2000@gmail.com>
2021-06-07 00:18:17 +0300

    1st + 2nd lessons

--- Program.cs
+++ Program.cs
@@ -4,9 +4,29 @@ namespace Delegates_And_Events
 {
     class MainClass
     {
+        delegate int BinaryOp(int a, int b); //signature
+
+
         public static void Main(string[] args)
         {
-            Console.WriteLine("First");
+            //variable based on delegate = delegate instance
+            //BinaryOp someOperation = new BinaryOp(Add);
+
+            BinaryOp someOperation  = Add;
+                     someOperation += Mul;
+
+            Console.WriteLine(someOperation(10, 10));
+        }
+
+
+        static int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        static int Mul(int a, int b)
+        {
+            return a * b;
         }
     }
 }

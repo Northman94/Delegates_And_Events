ec07502e1dec8306b026cb9a40f8d8029ca6606a
Northman94 <Zhandarm2000@gmail.com>
2021-06-15 23:21:03 +0300

    L1 changes due to old notes

--- Program.cs
+++ Program.cs
@@ -4,27 +4,34 @@ namespace Delegates_And_Events
 {
     class MainClass
     {
-        delegate int BinaryOp(int a, int b); //signature
+        delegate int BinaryOp(int a, int b); //Определили BinaryOp это [тип делегата]
 
 
         public static void Main(string[] args)
         {
-            //variable based on delegate = delegate instance
+            //Переменная с типом делегата, хранящая ссылку на метод:
+            BinaryOp someOperation  = Add; // variable based on delegate = delegate instance;
+                     someOperation += Mul; //Добавление нового метода в список;
+            //Все последующие методы, кроме первого, добавляются в список через "+=";
+
+
+            //Выше упрощенный вариант написания этого:
             //BinaryOp someOperation = new BinaryOp(Add);
+            // Помещение ссылки на метод в переменную +
+            // создание обьекта-экземпляра [типа делегата]
 
-            BinaryOp someOperation  = Add;
-                     someOperation += Mul;
 
+            //Вызов, который запустит очередь методов:
             Console.WriteLine(someOperation(10, 10));
         }
 
 
-        static int Add(int a, int b)
+        static int Add(int a, int b) //Будет ругаться, если возвращаемое значение не int
         {
             return a + b;
         }
 
-        static int Mul(int a, int b)
+        static int Mul(int a, int b) 
         {
             return a * b;
         }
